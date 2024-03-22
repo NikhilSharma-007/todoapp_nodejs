f0413e7037276ef42fc467c7e0075b63d1a74ebe
Nikhil Sharma <76101007+NikhilSharma-007@users.noreply.github.com>
2024-03-23 04:39:31 +0530

    Update app.js[cors origin]

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*',
+    origin: "https://todoapp-react-psi.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
